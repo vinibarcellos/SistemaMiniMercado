7dc9e8ac1662dce71bb8df4d5906012c02327efc
vinibarcellos <v.nitibailoff@gmail.com>
2025-10-23 19:51:07 -0300

    VendasService

--- src/vendas/IVendasService.java
+++ src/vendas/IVendasService.java
@@ -1,5 +1,7 @@
 package vendas;
 
+ //Oi vreno
+
 public interface IVendasService {
     void registrarVenda(Venda venda);
 }
--- src/vendas/VendasService.java
+++ src/vendas/VendasService.java
@@ -1,5 +1,7 @@
 package vendas;
 
+    //Oi vreno
+
 import java.util.HashMap;
 import java.util.Map;
 
